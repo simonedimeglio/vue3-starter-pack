7dff2c10115233edb28ba1b1968a33b84820ff0d
smne <78272736+simonedimeglio@users.noreply.github.com>
2024-06-25 09:26:20 +0200

    Add files via upload

---
+++ esercizi/CardUtente.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+    nome: String,
+    cognome: String,
+    mail: String,
+    ruolo: String
+})
+</script>
+
+<template>
+    <div>
+        <h2>{{ nome }} {{ cognome }}</h2>
+        <p>Email: {{ mail }}</p>
+        <p>Ruolo: {{ ruolo }}</p>
+    </div>
+</template>
---
+++ esercizi/Esercizio1.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+import CardUtente from './CardUtente.vue';
+
+const utenti = ref([
+    { id: 1, nome: 'Bob', cognome: 'Mufasa', mail: 'mufasa@gmail.com', ruolo: 'USER' },
+    { id: 2, nome: 'Patrik', cognome: 'Stella', mail: 'stellina@acqua.com', ruolo: 'ADMIN' },
+    { id: 3, nome: 'Spongebob', cognome: 'Squarepants', mail: 'spugna@arrembaggio.com', ruolo: 'USER' },
+    { id: 4, nome: 'Freddy', cognome: 'Mercurio', mail: 'termometro@musicale.com', ruolo: 'ADMIN' },
+])
+
+const filtroAttuale = ref('tutti')
+</script>
+
+<template>
+    <div>
+        <h1>Lista Utenti</h1>
+
+        <div>
+            <button @click="filtroAttuale = 'tutti'">Mostra tutti</button>
+            <button @click="filtroAttuale = 'user'">Mostra solo gli user</button>
+            <button @click="filtroAttuale = 'admin'">Mostra solo gli admin</button>
+        </div>
+
+        <div v-for="utente in utenti" :key="utente.id">
+            <CardUtente v-if="filtroAttuale === 'tutti' || filtroAttuale === utente.ruolo.toLowerCase()"
+                :nome="utente.nome" :cognome="utente.cognome" :mail="utente.mail" :ruolo="utente.ruolo" />
+        </div>
+    </div>
+</template>
+
